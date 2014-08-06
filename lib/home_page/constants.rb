012d9ca4294d441953f167d359c8aa20664d1c0e
AdnanZi <adnan.zildzic@gmail.com>
2014-08-06 17:11:50 +0200

    Added module for constants continued

---
+++ lib/home_page/constants.rb
@@ -0,0 +1,22 @@
+module Constants
+	ABOUT_TITLE = :About
+	CONTACT_TITLE = :Contact
+	EDIT_ABOUT_TITLE = 'Edit About'
+	EDIT_ABOUT_BUTTON = :Edit
+	EDIT_CONTACT_TITLE = 'Edit Contact'
+	EDIT_CONTACT_BUTTON = :Edit
+	BLOG_TITLE = 'Welcome to my blog!'
+	BLOG_DESCRIPTION = 'This is my developer\'s home page and blog.'
+	EDIT_BLOG_BUTTON = :Edit
+	DELETE_BLOG_BUTTON = :Delete
+	INFO_FORM_BUTTON = :Save
+	POST_FORM_BUTTON = :Save
+	EDIT_POST_TITLE = 'Edit post'
+	CREATE_POST_TITLE = 'Create post'
+	PAGE_TITLE = 'Adnan Zildzic blog'
+	ABOUT_BUTTON = :About
+	CONTACT_BUTTON = :Contact
+	NEW_POST_BUTTON = 'New post'
+	FOOTER_DESCRIPTION = 'Page built by its author, using Ruby on Rails and Bootstrap.'
+	FOOTER_TO_TOP = 'Back to top'
+end
